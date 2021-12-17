cdf8563a8e71087042364f73510ef14db71969f1
Владислав <vladusa146@gmail.com>
2021-12-17 15:45:59 +0300

    Добавьте файлы проекта.

---
+++ ButtonTutorial/ButtonTutorial/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ButtonTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "НАЖМИ НА МЕНЯ ЕЩЁ РАЗ!!!!!!!!!!!!";
+        }
+    }
+}
